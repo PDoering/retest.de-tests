8a692a84872915d34f5969c26e6a0aa266477185
Jeremias Roessler <roessler@retest.de>
2019-08-23 00:03:54 +0200

    Make rect(0px, 0px, 0px, 0px) and rect(0px 0px 0px 0px) equal...

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -7,3 +7,11 @@
 
 // You can find more details and example rules at: 
 // https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
+
+function matches(element, diff) {
+	// make clip: expected="rect(0px, 0px, 0px, 0px)", actual="rect(0px 0px 0px 0px)" equal
+	if (diff.key == "clip") {
+		return diff.expected.replace(/,/g, '') === diff.actual.replace(/,/g, '');
+	}
+	return false;
+}
