85bbd2024f357c37b477ea61f753873ebeefb56a
Enes Özel <enes.oezel@retest.de>
2019-08-23 10:38:04 +0200

    Improve test method

--- src/test/java/de/retest/recheck/RetestWebpageTest.java
+++ src/test/java/de/retest/recheck/RetestWebpageTest.java
@@ -3,7 +3,7 @@ package de.retest.recheck;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,38 +15,30 @@ class RetestWebpageTest {
 
 	@BeforeAll
 	static void setup() {
-		final var chromeOpts = new ChromeOptions();
-		chromeOpts.addArguments( "--headless", "--no-sandbox", "--window-size=1200,800" );
-		driver = new ChromeDriver( chromeOpts );
+		final var opts = new ChromeOptions() //
+				.addArguments( // 
+						"--headless", //
+						"--no-sandbox", //
+						"--window-size=1200,800" );
+		driver = new ChromeDriver( opts );
 
-		final var recheckOpts = RecheckOptions.builder() //
+		final var recheckOptions = RecheckOptions.builder() //
 				.reportUploadEnabled( true ) //
 				.build();
-		re = new RecheckImpl( recheckOpts );
+		re = new RecheckImpl( recheckOptions );
 	}
 
 	@ParameterizedTest
-	@ValueSource( strings = { "https://retest.de", "https://retest.de/devtesters", "https://retest.de/managers",
-			"https://retest.de/product-overview", "https://retest.de/recheck-web-chrome-extension",
-			"https://retest.de/recheck-open-source", "https://retest.de/rehub", "https://retest.de/review",
-			"https://retest.de/request-demo", "https://retest.de/faqs", "https://retest.de/about-us",
-			"https://retest.de/team", "https://retest.de/jobs", "https://retest.de/news",
-			"https://retest.de/contact-us", "https://assets.retest.org/releases/review.html",
-			"https://www.retest.de/feature-unbreakable-selenium", "http://garkbit.prod.cloud.retest.org/dashboard" } )
-	void test( final String link ) throws Exception {
-		re.startTest( "retest-de" );
+	@MethodSource( "de.retest.recheck.util.WebPageFactory#getLinks" )
+	void allRetestPages( final String link ) throws Exception {
+		re.startTest( link.substring( link.indexOf( "//" ) + 2 ).replaceAll( "[\\/\\-]", "." ) );
 
 		driver.get( link );
 		Thread.sleep( 1000 );
 
-		re.check( driver, getCheckName( link ) );
+		re.check( driver, "check" );
 
 		re.capTest();
-	}
-
-	String getCheckName( final String link ) {
-		final String checkName = link.substring( link.lastIndexOf( "/" ) + 1 );
-		return checkName.equals( "retest.de" ) ? "index" : checkName;
 	}
 
 	@AfterAll
