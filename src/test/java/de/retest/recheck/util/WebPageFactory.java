aa6aad0e954d58261b5d3e1fe44ccd77691215db
Enes Özel <enes.oezel@retest.de>
2019-12-18 15:13:37 +0100

    Add new pages

--- src/test/java/de/retest/recheck/util/WebPageFactory.java
+++ src/test/java/de/retest/recheck/util/WebPageFactory.java
@@ -11,19 +11,27 @@ public class WebPageFactory {
 				"https://retest.de/managers", //
 				"https://retest.de/product-overview", //
 				"https://retest.de/recheck-web-chrome-extension", //
-				"https://retest.de/recheck-open-source", //
+				"https://retest.de/recheck-web-open-source/", //
 				"https://retest.de/rehub", //
 				"https://retest.de/review", //
-				"https://retest.de/request-demo", //
+				"https://retest.de/demo-req/", //
 				"https://retest.de/faqs", //
 				"https://retest.de/about-us", // 
 				"https://retest.de/team", //
 				"https://retest.de/jobs", //
 				"https://retest.de/news", //
 				"https://retest.de/contact-us", //
+				"https://retest.de/blog/", //
+				"https://retest.de/upcoming-events/", //
+				"https://retest.de/who-is-retest/", //
+				"https://retest.de/legal-notice/", //
+				"https://retest.de/privacy-policy/", //
+				"https://retest.de/terms-of-service/", //
+				"https://retest.de/ai-based-test-generation/", //
 				"https://assets.retest.org/releases/review.html", //
 				"https://retest.de/feature-unbreakable-selenium", //
-				"http://garkbit.prod.cloud.retest.org/dashboard" //
+				"http://garkbit.prod.cloud.retest.org/dashboard", //
+				"https://assets.retest.org/retest/ci/index.html" //
 		);
 	}
 }
