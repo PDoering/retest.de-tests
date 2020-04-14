de0945a8f1b58fddc75b5891b2f2b4d9822659b6
lemoncurry <agape.anna@gmail.com>
2020-04-14 18:51:49 +0200

    Update list of web pages

--- src/test/java/de/retest/recheck/util/WebPageFactory.java
+++ src/test/java/de/retest/recheck/util/WebPageFactory.java
@@ -9,18 +9,23 @@ public class WebPageFactory {
 				"https://retest.de", // 
 				"https://retest.de/devtesters", //
 				"https://retest.de/managers", //
+				"https://retest.de/enterprise-solution/", //
 				"https://retest.de/product-overview", //
 				"https://retest.de/recheck-web-chrome-extension", //
-				"https://retest.de/recheck-open-source", //
+				"https://retest.de/recheck-web-open-source/", //
 				"https://retest.de/rehub", //
 				"https://retest.de/review", //
-				"https://retest.de/request-demo", //
+				"https://retest.de/ai-based-test-generation/", //
+				"https://retest.de/demo-req/", //
 				"https://retest.de/faqs", //
+				"https://retest.de/supported-os-devices/", //
+				"https://retest.de/cross-browser-testing/", //
 				"https://retest.de/about-us", // 
 				"https://retest.de/team", //
 				"https://retest.de/jobs", //
 				"https://retest.de/news", //
 				"https://retest.de/contact-us", //
+				"https://retest.de/blog/", //
 				"https://assets.retest.org/releases/review.html", //
 				"https://retest.de/feature-unbreakable-selenium", //
 				"http://garkbit.prod.cloud.retest.org/dashboard" //
